94187d6c9db2018e87516a18128c8604a5aed57e
Morgan Nance <morganlnance@gmail.com>
2016-09-20 21:51:53 -0400

    added more questions. Fixed cmd.set_view. Finished 5c

--- PNA/problem_set2/script_5c.py
+++ PNA/problem_set2/script_5c.py
@@ -16,10 +16,31 @@ cmd.load("1ij1.pdb")
 cmd.show( "cartoon" )
 cmd.hide( "lines" )
 
-# show waters as sticks
-cmd.show( "spheres", "resn hoh" )
+# show only the buried waters as spheres, otherwise remove the waters
+# C204, B203, A202, A203
+cmd.select( "delete_waters", "resn hoh and not ( resi 203+202 and chain A + resi 203 and chain B + resi 204 and chain C )" )
+cmd.remove( "delete_waters" )
+cmd.delete( "delete_waters" )
+cmd.select( "buried_waters", "resn hoh" )
+cmd.show( "spheres", "buried_waters" )
 cmd.set( "sphere_scale", 0.5 )
-cmd.h_add( "resn hoh" )
+cmd.h_add( "buried_waters" )
+cmd.deselect()
 
-# select buried waters
-# C204, B203, A203, A202
+# select the Thr residues around the buried waters
+cmd.select( "thr_residues", "resi 12 and chain A + resi 12 and chain B + resi 12 and chain C" )
+cmd.show( "sticks", "thr_residues and !(name c+o+n )" )
+cmd.deselect()
+
+# get the distances between the O of HOH and O of the 3 Thr residues
+cmd.dist( "thr_hoh_A", "/1ij1//A/THR`12/OG1", "/1ij1//A/HOH`202/O" )
+cmd.dist( "thr_hoh_B", "/1ij1//B/THR`12/OG1", "/1ij1//B/HOH`203/O" )
+cmd.dist( "thr_hoh_C", "/1ij1//C/THR`12/OG1", "/1ij1//C/HOH`204/O" )
+
+# set the view
+cmd.set_view ( "0.808693528, 0.356420070, 0.467955768,\
+                   -0.388590366, 0.920925915, -0.029888431,\
+                   -0.441604972, -0.157672778, 0.883246481,\
+                   0.000000000, 0.000000000, -50.762001038,\
+                   -2.018598557, -0.170596123, 11.568523407,\
+                   15.002751350, 86.521240234, -20.000000000" )
