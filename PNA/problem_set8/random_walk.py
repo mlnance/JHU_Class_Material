e56e6a70383115bd667d50564e03cb678899a7b0
Morgan Nance <morganlnance@gmail.com>
2016-11-05 16:48:27 -0400

    finishing up the random_walk question p8 q5

--- PNA/problem_set8/random_walk.py
+++ PNA/problem_set8/random_walk.py
@@ -6,7 +6,6 @@ __question__="ps8_q5"
 python random_walk.py
 '''
 
-
 # imports
 from random import choice
 
@@ -17,16 +16,39 @@ steps = [ -1, 0, 1 ]
 # time to get to a location 100 steps away
 location = 0
 nsteps = 0
+tensteps_velocities = []
+hundredsteps_velocities = []
 while abs( location ) < 100:
     location += choice( steps )
     nsteps += 1
-print "\nIt took %s milliseconds to randomly walk 100 steps in one direction" %( nsteps * ( 10**-3 ) )
+    # calculating velcoity every 10 steps
+    if nsteps % 10 == 0:
+        tensteps_velocities.append( float( abs( location ) ) / ( nsteps * ( 10**-3 ) ) )
+    # calculating velcoity every 100 steps
+    if nsteps % 100 == 0:
+        hundredsteps_velocities.append( float( abs( location ) ) / ( nsteps * ( 10**-3 ) ) )
+print "\n### 100 steps away"
+print "It took %s milliseconds to randomly walk 100 steps in one direction" %( nsteps * ( 10**-3 ) )
+print "   The average velocity using 10-step measurements was %s steps/msec" %round( ( sum( tensteps_velocities ) / len( tensteps_velocities ) ), 3 )
+print "   The average velocity using 100-step measurements was %s steps/msec\n" %round( ( sum( hundredsteps_velocities ) / len( hundredsteps_velocities ) ), 3 )
+
 
 
 # time to get to a location 1000 steps away
 location = 0
 nsteps = 0
+tensteps_velocities = []
+hundredsteps_velocities = []
 while abs( location ) < 1000:
     location += choice( steps )
     nsteps += 1
-print "It took %s milliseconds to randomly walk 1000 steps in one direction\n" %( nsteps * ( 10**-3 ) )
+    # calculating velcoity every 10 steps
+    if nsteps % 10 == 0:
+        tensteps_velocities.append( float( abs( location ) ) / ( nsteps * ( 10**-3 ) ) )
+    # calculating velcoity every 100 steps
+    if nsteps % 100 == 0:
+        hundredsteps_velocities.append( float( abs( location ) ) / ( nsteps * ( 10**-3 ) ) )
+print "### 1000 steps away"
+print "It took %s milliseconds to randomly walk 1000 steps in one direction" %( nsteps * ( 10**-3 ) )
+print "   The average velocity using 10-step measurements was %s steps/msec" %round( ( sum( tensteps_velocities ) / len( tensteps_velocities ) ), 3 )
+print "   The average velocity using 100-step measurements was %s steps/msec\n" %round( ( sum( hundredsteps_velocities ) / len( hundredsteps_velocities ) ), 3 )
